588b133da7970ec114decb64969708f6d43ab25c
wonderall <skyhybooks@gmail.com>
2023-10-30 20:28:36 +0900

    1030

---
+++ python100/1day/1day_banNaming.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator.")
+city = input("which city didyou grow up in? ")
+pet =  input("What is the name of a pet? ")
+print("Your band name could be "+city+"-"+pet)
---
+++ python100/2day/data_types.py
@@ -0,0 +1,7 @@
+#String
+str1 = "Hello"
+print(str1[0])
+print("123"+"456")
+print(len(str1))
+#Integer
+print(123+456)
